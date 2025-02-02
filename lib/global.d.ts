5d8ab9ab0ebc7d60e644f3cfa003dc8a71413468
qqharry21 <qqharry21@gmail.com>
2025-02-02 13:27:15 +0800

    fix: add global type

---
+++ lib/global.d.ts
@@ -0,0 +1,10 @@
+import * as Types from '@prisma/client';
+export { Types };
+
+export {};
+declare global {
+  interface IntlMessages extends Messages, ZodMessages {}
+  interface PropsWithChildren {
+    children: React.ReactNode | React.ReactNode[];
+  }
+}
