56f6ce7d99b97770750ad169925809ed2f7a85d1
qqharry21 <qqharry21@gmail.com>
2025-02-02 13:28:10 +0800

    refactor: add mouse-move-effect component

---
+++ components/mouse-move-effect.tsx
@@ -0,0 +1,36 @@
+'use client';
+
+import { useTheme } from 'next-themes';
+import { useEffect, useState } from 'react';
+
+export default function MouseMoveEffect() {
+  const { theme } = useTheme();
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    if (theme === 'dark') {
+      const handleMouseMove = (event: MouseEvent) => {
+        setMousePosition({ x: event.clientX, y: event.clientY });
+      };
+
+      window.addEventListener('mousemove', handleMouseMove);
+
+      return () => {
+        window.removeEventListener('mousemove', handleMouseMove);
+      };
+    }
+  }, [theme]);
+
+  if (theme === 'light') {
+    return null;
+  }
+
+  return (
+    <div
+      className='pointer-events-none fixed inset-0 z-30 transition-opacity duration-300'
+      style={{
+        background: `radial-gradient(300px at ${mousePosition.x}px ${mousePosition.y}px, rgba(29, 78, 216, 0.15), transparent 80%)`,
+      }}
+    />
+  );
+}
