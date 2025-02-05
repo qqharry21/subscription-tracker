bdf91f6e9e5c1d17cbcdb10beea77fa5db48f219
qqharry21 <qqharry21@gmail.com>
2025-02-05 14:52:07 +0800

    chore: update github workflow

--- types/enums.ts
+++ types/enums.ts
@@ -1,28 +1,9 @@
 // THIS FILE IS AUTO-GENERATED
 
-export enum Category {
-  FOOD = "FOOD",
-  TRANSPORTATION = "TRANSPORTATION",
-  SHOPPING = "SHOPPING",
-  ENTERTAINMENT = "ENTERTAINMENT",
-  BILLS = "BILLS",
-  HEALTH = "HEALTH",
-  LEARNING = "LEARNING",
-  INVESTMENT = "INVESTMENT",
-  INSURANCE = "INSURANCE",
-  RENTAL = "RENTAL",
-  TAXES = "TAXES",
-  CHARITY = "CHARITY",
-  GIFT = "GIFT",
-  FAMILY = "FAMILY",
-  TRAVEL = "TRAVEL",
-  OTHER = "OTHER"
-}
-
 export enum Currency {
   USD = "USD",
   JPY = "JPY",
-  TWD = "TWD"
+  TWD = "TWD",
 }
 
 export enum Frequency {
@@ -30,5 +11,5 @@ export enum Frequency {
   DAILY = "DAILY",
   WEEKLY = "WEEKLY",
   MONTHLY = "MONTHLY",
-  ANNUALLY = "ANNUALLY"
+  ANNUALLY = "ANNUALLY",
 }
